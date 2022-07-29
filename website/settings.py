596fb4a964fd77d8729ada8aabe94b5ff78fe378
Laura Lima <lauralimaufam@gmail.com>
2022-07-29 03:16:52 -0300

    update on settings

--- website/settings.py
+++ website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
